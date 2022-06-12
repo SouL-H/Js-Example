3605d956a3917db741f7e1d5fc3444211319bdf9
SouL-H <mhmtaydinw@gmail.com>
2022-06-12 07:45:19 +0300

    querySelector

--- DOM/dom.js
+++ DOM/dom.js
@@ -2,3 +2,9 @@ console.log(document.URL)
 console.log(document.location)
 console.log(document.location.pathname)
 console.log(document.getElementById("test").innerText)
+
+console.log(document.querySelector("ul#test2>li>a").innerHTML)//ul içindeki li içindeki a
+
+let a = document.querySelector("#dataSelect")
+a.style.color="red"
+console.log(a.text)
