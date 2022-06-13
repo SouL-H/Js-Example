3a91ce531c845d374907426d56315159efc5361b
SouL-H <mhmtaydinw@gmail.com>
2022-06-13 11:16:33 +0300

    FatArrow

---
+++ FatArrow/fatArrow.js
@@ -0,0 +1,21 @@
+function hello(x){
+    console.log(x)
+}
+
+
+hello("Hello")
+
+
+const helloV1=(x)=>{
+    console.log(x)
+}
+
+helloV1("Hello")
+
+const helloV2=(x,y)=>{
+    console.log(x+y)
+    return x+y
+}
+
+
+let a= helloV2(1,2)
